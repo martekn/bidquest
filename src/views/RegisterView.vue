215cedfe50c9043cbf3a9692f5f580ac150f4382
Marte <marte.mk@hotmail.com>
2024-03-08 20:43:03 +0000

    Setup template views

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="grid place-items-center">
+      <h1>Work in progress</h1>
+      <p>
+        The website is currently under development and will launch soon. Come back and check it out
+      </p>
+    </div>
+  </main>
+</template>
