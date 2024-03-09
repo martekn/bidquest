d58f6e2ce42e49ca39a6336df1f1c369e920c2d3
Marte <marte.mk@hotmail.com>
2024-03-09 17:52:52 +0000

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      black: "#181818",
+      white: "#ffffff",
+      primary: {
+        300: "#7B283C",
+        400: "#672333",
+        500: "#602433"
+      },
+      grey: {
+        200: "#FAFAFA",
+        300: "#DFDFDF",
+        400: "#8A8A8A",
+        500: "#666666"
+      },
+      green: {
+        300: "#EBFFEB",
+        400: "#11A111"
+      },
+      red: {
+        300: "#AF1320",
+        400: "#FFF0F1"
+      }
+    }
   },
   plugins: [require("tailwindcss-animated")]
 };
