ac14a2669c9538ac7492ab4255b7c406322af40a
Marte <marte.mk@hotmail.com>
2024-03-09 19:13:08 +0000

    Add spacing tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,20 @@ export default {
       "4xl": "2.154rem",
       "5xl": "2.726rem",
       "6xl": "3.063rem"
+    },
+    spacing: {
+      1: "0.125rem",
+      2: "0.25rem",
+      3: "0.5rem",
+      4: "0.75rem",
+      5: "1rem",
+      6: "1.5rem",
+      7: "2rem",
+      8: "2.5rem",
+      9: "3rem",
+      10: "4rem",
+      11: "6rem",
+      12: "8rem"
     }
   },
   plugins: [require("tailwindcss-animated")]
