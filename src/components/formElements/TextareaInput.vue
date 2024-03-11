bf479b8a7e94d8257e270e2c9ebe638c49d452c6
Marte <marte.mk@hotmail.com>
2024-03-11 11:48:32 +0000

    Create textarea component

---
+++ src/components/formElements/TextareaInput.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  label: { type: String, default: "" },
+  modelValue: {
+    type: String,
+    default: ""
+  },
+  name: { type: String },
+  id: { type: String }
+});
+</script>
+
+<template>
+  <div class="input-group">
+    <label :for="id">{{ label }}</label>
+
+    <textarea
+      :id="id"
+      :name="name"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
