9f8241c85c9595a4de78fd9c7838c06b183d0562
Marte <marte.mk@hotmail.com>
2024-03-13 19:23:29 +0000

    Create search form

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+let searchInput = ref("");
+</script>
+
+<template>
+  <form role="search" class="relative text-grey-500 focus-within:text-black">
+    <label for="searchInput" class="sr-only">Search</label>
+    <input type="text" class="" v-model="searchInput" />
+
+    <button
+      @click="
+        () => {
+          $router.push({ path: '/search/' + searchInput });
+        }
+      "
+      :class="{ 'pointer-events-none': !!searchInput === false }"
+      :disabled="!!searchInput === false"
+      class="focus-visible:inner-border-off absolute right-3 top-1/2 grid h-7 w-7 -translate-y-1/2 transform place-items-center self-stretch rounded inner-border-black hover:bg-grey-300/50 focus-visible:bg-primary-400 focus-visible:text-white focus-visible:outline-none"
+    >
+      <MagnifyingGlassIcon class="h-[1.3rem] w-[1.3rem]" />
+    </button>
+  </form>
+</template>
