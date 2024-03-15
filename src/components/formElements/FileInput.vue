7c85c2278e6feefb1e1b8416a7acde64efe3d3c8
Marte <marte.mk@hotmail.com>
2024-03-15 20:26:05 +0000

    Create file input component

---
+++ src/components/formElements/FileInput.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  label: { type: String, default: "Add image" },
+  labelClasses: { type: String, default: "button button-secondary" },
+  name: { type: String, default: "file-input" },
+  id: { type: String, default: "file-input" },
+  multiple: { type: Boolean, default: false }
+});
+</script>
+
+<template>
+  <div class="flex w-full items-center justify-center">
+    <label :for="id" :class="labelClasses">
+      {{ label }}
+      <input
+        :id="id"
+        :name="name"
+        type="file"
+        @change="$emit('change', $event.target.files)"
+        class="hidden"
+        :multiple="multiple"
+      />
+    </label>
+  </div>
+</template>
