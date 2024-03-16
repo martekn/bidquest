bed2bf5476a38fd1e58cbb852fff650b7df574ae
Marte <marte.mk@hotmail.com>
2024-03-16 14:07:36 +0000

    Create popover component

---
+++ src/components/popover/PopoverItem.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const props = defineProps({
+  description: String,
+  as: {
+    type: String,
+    default: "a"
+  },
+  routerLink: { type: Boolean, default: false },
+  path: { type: String, default: "" }
+});
+
+const classes =
+  "block w-full rounded px-5 py-3 text-left text-sm outline-none hover:bg-grey-200 hover:text-black hover:ring-0 focus-visible:inner-border-2 focus-visible:inner-border-black";
+</script>
+
+<template>
+  <li>
+    <component v-if="!routerLink" :is="props.as" :class="classes" v-bind="$attrs">
+      <slot />
+    </component>
+    <RouterLink v-else :class="classes" :to="props.path"><slot /></RouterLink>
+  </li>
+</template>
---
+++ src/components/popover/PopoverMenu.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+
+const props = defineProps({
+  align: {
+    type: String,
+    default: "left",
+    validator: (value) => ["left", "right"].includes(value)
+  },
+  width: { type: String, default: "10rem" }
+});
+</script>
+<template>
+  <Popover as="div" class="group relative inline-block text-left">
+    <div>
+      <PopoverButton as="template"><slot /></PopoverButton>
+    </div>
+
+    <PopoverPanel
+      :class="{
+        'right-0': props.align === 'right',
+        'left-0': props.align === 'left'
+      }"
+      class="absolute z-10 mt-2 max-w-[80vw] divide-y divide-grey-300 rounded border border-grey-300 bg-white text-grey-500 shadow-md shadow-black/5 focus:outline-none"
+      :style="{ width: props.width }"
+    >
+      <slot name="items" />
+    </PopoverPanel>
+  </Popover>
+</template>
