15914adfce86731c6ba00cdd54525847e8ad686c
Marte <marte.mk@hotmail.com>
2024-03-19 09:42:33 +0000

    Add confirmation dialog component

---
+++ src/components/ConfirmationDialog.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel, DialogTitle, DialogDescription } from "@headlessui/vue";
+
+defineProps({
+  isOpen: { type: Boolean, default: false },
+  title: { type: String, default: "Are you sure?" },
+
+  ctaText: { type: String, default: "Delete" }
+});
+
+const panel = ref();
+</script>
+
+<template>
+  <Dialog :open="isOpen" @close="$emit('close')" :initialFocus="panel" class="relative z-50">
+    <div class="fixed inset-0 flex w-screen items-center justify-center">
+      <div class="fixed inset-0 bg-black/75" aria-hidden="true" />
+      <DialogPanel
+        ref="panel"
+        class="z-10 grid w-full max-w-lg overflow-y-auto rounded bg-white px-6 pt-6 shadow-md shadow-black/10"
+      >
+        <DialogTitle class="text-semibold font-accent text-lg">{{ title }}</DialogTitle>
+        <DialogDescription class="mt-2 text-grey-500"> <slot /></DialogDescription>
+
+        <div class="mt-6 flex flex-row-reverse gap-6 border-t border-t-grey-300 py-4">
+          <button class="button button-primary button-sm" @click="$emit('confirm')">
+            {{ ctaText }}
+          </button>
+          <button @click="$emit('close')" class="link link-secondary ml-auto">Cancel</button>
+        </div>
+      </DialogPanel>
+    </div>
+  </Dialog>
+</template>
