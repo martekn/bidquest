bd4bb5765d9fd4fbd38cba7b250ce9115b1898b2
Marte <marte.mk@hotmail.com>
2024-04-02 12:29:38 +0000

    Allow users to register a new account

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,24 +1,110 @@
 <script setup>
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import PasswordInput from "@/components/formElements/PasswordInput.vue";
 import TextInput from "@/components/formElements/TextInput.vue";
+import LoadingIndicator from "@/components/LoadingIndicator.vue";
+import { XCircleIcon } from "@heroicons/vue/20/solid";
+import { placeholderUserImageUrl } from "@/consts/placeholderUserImageUrl";
+import { register } from "@/api/auth/register";
+import { Validate } from "@/helper/Validate";
 
+const isLoading = ref(false);
+const apiError = reactive([]);
 const registerBody = reactive({ name: "", email: "", password: "" });
+
+const nameField = reactive({
+  isError: false,
+  error: ""
+});
+
+const emailField = reactive({
+  isError: false,
+  error: ""
+});
+
+const passwordField = reactive({
+  isError: false,
+  error: ""
+});
+
+const submit = async () => {
+  try {
+    isLoading.value = true;
+    registerBody.avatar = { url: placeholderUserImageUrl, alt: "placeholder image" };
+
+    await register(registerBody);
+  } catch (error) {
+    isLoading.value = false;
+    apiError.push([...error.errors]);
+    nameField.isError = true;
+    emailField.isError = true;
+    passwordField.isError = true;
+  }
+};
+
+const validate = () => {
+  console.log(registerBody);
+  nameField.error = "";
+  emailField.error = "";
+  passwordField.error = "";
+  apiError.length = 0;
+
+  nameField.error = Validate.username(registerBody.name);
+  emailField.error = Validate.email(registerBody.email);
+  passwordField.error = Validate.password(registerBody.password);
+
+  nameField.isError = nameField.error ? true : false;
+  emailField.isError = emailField.error ? true : false;
+  passwordField.isError = passwordField.error ? true : false;
+
+  if (!nameField.error && !emailField.error && !passwordField.error) {
+    submit();
+  }
+};
 </script>
 
 <template>
-  <main class="bg-straws-pattern grid flex-1 place-items-center bg-opacity-50 bg-repeat">
+  <main class="grid flex-1 place-items-center bg-opacity-50 bg-straws-pattern bg-repeat">
     <form
       @submit.prevent
-      class="mx-auto grid w-full gap-7 border border-grey-300 p-5 sm:max-w-lg sm:rounded sm:bg-white sm:p-9 sm:shadow-md sm:shadow-black/10"
+      class="mx-auto grid w-full gap-6 border border-grey-300 p-5 sm:max-w-lg sm:rounded sm:bg-white sm:p-9 sm:shadow-md sm:shadow-black/10"
     >
       <h1 class="text-lg md:text-xl">Create account</h1>
+      <div v-if="apiError.length > 0" class="flex w-full items-start gap-3 rounded bg-red-300 p-5">
+        <XCircleIcon class="my-1 h-5 w-5 flex-shrink-0 text-red-400" />
 
-      <TextInput v-model="registerBody.name" label="Username" type="test" />
-      <TextInput v-model="registerBody.email" label="Email" type="email" />
-      <PasswordInput v-model="registerBody.password" label="Password" />
+        <div class="flex flex-col gap-2">
+          <p class="font-semibold leading-tight">We could not create your account</p>
+          <ul class="list-inside list-disc">
+            <li v-for="[error, index] in apiError" :key="index">{{ error.message }}</li>
+          </ul>
+        </div>
+      </div>
+      <TextInput
+        v-model="registerBody.name"
+        :error="nameField.error"
+        :is-error="nameField.isError"
+        label="Username"
+        type="test"
+      />
+      <TextInput
+        v-model="registerBody.email"
+        :error="emailField.error"
+        :is-error="emailField.isError"
+        label="Email"
+        type="email"
+      />
+      <PasswordInput
+        v-model="registerBody.password"
+        :error="passwordField.error"
+        :is-error="passwordField.isError"
+        label="Password"
+      />
 
-      <button class="button button-primary">Create</button>
+      <button @click="validate" class="button button-primary">
+        <span v-if="!isLoading"> Create </span>
+        <LoadingIndicator v-else color="light" />
+      </button>
       <span class="border-t border-t-grey-300 pt-6 leading-tight text-grey-500"
         >Already have an account?
         <RouterLink class="link link-secondary" to="/login">login</RouterLink></span
