ded5428f94f9deeb58e21f0242d4b03d8004c405
Marte <marte.mk@hotmail.com>
2024-04-05 09:24:45 +0000

    Add notification component

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Notifications from "notiwind";
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(Notifications);
 app.mount("#app");
