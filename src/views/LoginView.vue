466c32e7fd82bc4a1e401ec6fe740fdcd5aea3fe
Marte <marte.mk@hotmail.com>
2024-04-06 11:07:58 +0000

    Update pages to use ErrorDialog component

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,9 +3,9 @@ import { reactive, ref } from "vue";
 import PasswordInput from "@/components/formElements/PasswordInput.vue";
 import LoadingIndicator from "@/components/LoadingIndicator.vue";
 import TextInput from "@/components/formElements/TextInput.vue";
-import { XCircleIcon } from "@heroicons/vue/20/solid";
 import { login } from "@/api/auth/login";
 import { Validate } from "@/helper/Validate";
+import ErrorDialog from "@/components/ErrorDialog.vue";
 
 const isLoading = ref(false);
 const apiError = reactive([]);
@@ -59,20 +59,12 @@ const validate = () => {
       class="mx-auto grid w-full gap-6 p-5 sm:max-w-lg sm:rounded sm:border sm:border-grey-300 sm:bg-white sm:p-9 sm:shadow-md sm:shadow-black/10"
     >
       <h1 class="text-lg md:text-xl">Login</h1>
-      <div
-        id="login-error"
-        v-if="apiError.length > 0"
-        class="flex w-full items-start gap-3 rounded bg-red-300 p-5"
-      >
-        <XCircleIcon class="my-1 h-5 w-5 flex-shrink-0 text-red-400" />
+      <ErrorDialog id="login-error" v-if="apiError.length > 0" title="We could not log you in">
+        <ul class="list-inside list-disc">
+          <li v-for="[error, index] in apiError" :key="index">{{ error.message }}</li>
+        </ul>
+      </ErrorDialog>
 
-        <div class="flex flex-col gap-2">
-          <p class="font-semibold leading-tight">We could not log you in</p>
-          <ul class="list-inside list-disc">
-            <li v-for="[error, index] in apiError" :key="index">{{ error.message }}</li>
-          </ul>
-        </div>
-      </div>
       <TextInput
         v-model="loginBody.email"
         :is-error="emailField.isError"
