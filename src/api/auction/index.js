5f82b1e4c59c57b837d96af0b487e4de33e40c65
Marte <marte.mk@hotmail.com>
2024-04-08 12:01:44 +0000

    Include bid fetch function

--- src/api/auction/index.js
+++ src/api/auction/index.js
@@ -1,7 +1,9 @@
 import { getAll } from "./getAll";
 import { getSingle } from "./getSingle";
+import { bid } from "./bid";
 
 export const auction = Object.freeze({
   getAll,
-  getSingle
+  getSingle,
+  bid
 });
