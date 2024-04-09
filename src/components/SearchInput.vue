d8b99af16d7d55e56bed15723e5e18d57f501a2f
Marte <marte.mk@hotmail.com>
2024-04-09 13:16:48 +0000

    Clear search input on navigating away from search

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,8 +1,21 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
+import { useRoute } from "vue-router";
 import router from "@/router";
+
+const route = useRoute();
 let searchInput = ref("");
+
+const search = (value) => {
+  router.push({ name: "search", params: { query: value } });
+};
+
+watch(route, (to) => {
+  if (to.name !== "search") {
+    searchInput.value = "";
+  }
+});
 </script>
 
 <template>
@@ -17,11 +30,7 @@ let searchInput = ref("");
     />
 
     <button
-      @click="
-        () => {
-          router.push({ name: 'search', params: { query: searchInput } });
-        }
-      "
+      @click="search(searchInput)"
       :class="{ 'pointer-events-none': !!searchInput === false }"
       :disabled="!!searchInput === false"
       class="focus-visible:inner-border-off absolute right-3 top-1/2 grid h-7 w-7 -translate-y-1/2 transform place-items-center self-stretch rounded transition-all duration-150 inner-border-black hover:bg-grey-300/50 focus-visible:bg-primary-400 focus-visible:text-white focus-visible:outline-none"
