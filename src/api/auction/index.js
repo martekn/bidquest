72559ce9d7c6e7fc647f28a601850ce42476508e
Marte <marte.mk@hotmail.com>
2024-04-12 11:56:14 +0000

    Add fetch function for searching

--- src/api/auction/index.js
+++ src/api/auction/index.js
@@ -2,10 +2,12 @@ import { getAll } from "./getAll";
 import { getSingle } from "./getSingle";
 import { bid } from "./bid";
 import { getByCategory } from "./getByCategory";
+import { search } from "./search";
 
 export const auction = Object.freeze({
   getAll,
   getSingle,
   bid,
-  getByCategory
+  getByCategory,
+  search
 });
