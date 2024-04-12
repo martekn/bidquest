4c73561d297a8beec0a0ca2a36e383beadb7e3cf
Marte <marte.mk@hotmail.com>
2024-04-12 11:55:43 +0000

    Add fetch by category function

--- src/api/auction/index.js
+++ src/api/auction/index.js
@@ -1,9 +1,11 @@
 import { getAll } from "./getAll";
 import { getSingle } from "./getSingle";
 import { bid } from "./bid";
+import { getByCategory } from "./getByCategory";
 
 export const auction = Object.freeze({
   getAll,
   getSingle,
-  bid
+  bid,
+  getByCategory
 });
