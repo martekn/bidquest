09dc409bd0ab7ee8a39675f69b44dd3e3cc6b753
Marte <marte.mk@hotmail.com>
2024-04-13 12:18:02 +0000

    Move navItems into consts folder

---
+++ src/consts/navItems.js
@@ -0,0 +1,17 @@
+export const navItems = [
+  { name: "+ Create auction", route: { name: "create" }, id: 1 },
+  { name: "All auctions", route: { name: "auctions" }, id: 2 },
+  { name: "Jewelry", route: { name: "category", params: { category: "jewelry" } }, id: 3 },
+  { name: "Art", route: { name: "category", params: { category: "art" } }, id: 4 },
+  { name: "Fashion", route: { name: "category", params: { category: "fashion" } }, id: 5 },
+  { name: "Electronics", route: { name: "category", params: { category: "electronics" } }, id: 6 },
+  { name: "Home", route: { name: "category", params: { category: "home" } }, id: 7 },
+  { name: "Toys", route: { name: "category", params: { category: "toys" } }, id: 8 },
+  { name: "Sports", route: { name: "category", params: { category: "sports" } }, id: 9 },
+  { name: "Vehicles", route: { name: "category", params: { category: "vehicles" } }, id: 10 },
+  {
+    name: "Miscellaneous",
+    route: { name: "category", params: { category: "miscellaneous" } },
+    id: 11
+  }
+];
