e394560401b6e56d48065283056a88d2c8299bec
Marte <marte.mk@hotmail.com>
2024-04-14 12:55:12 +0000

    Create avatar component

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  url: { type: String, default: "/avatar-placeholder.jpg" },
+  alt: { type: String, default: "" }
+});
+</script>
+<template>
+  <div
+    class="relative after:absolute after:inset-0 after:rounded after:border-2 after:border-black/10"
+  >
+    <img
+      class="zz- relative h-full w-full rounded object-cover"
+      :src="url"
+      :alt="alt"
+      onerror="this.onerror=null;this.src='/avatar-placeholder.jpg';"
+    />
+  </div>
+</template>
