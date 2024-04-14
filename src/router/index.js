f2abeb7242367dc6abea1763e6b6e2e364ccbb74
Marte <marte.mk@hotmail.com>
2024-04-14 13:39:38 +0000

    Implement active ended option for params

--- src/router/index.js
+++ src/router/index.js
@@ -102,7 +102,7 @@ const router = createRouter({
       meta: { requiresAuth: true },
       children: [
         {
-          path: ":view?",
+          path: ":view(active|ended)?",
           name: "profile",
           component: ProfileView
         },
