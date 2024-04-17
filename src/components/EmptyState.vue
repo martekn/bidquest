996869f3d3f948966729ee1015039f660f04473e
Marte <marte.mk@hotmail.com>
2024-04-17 16:56:06 +0000

    Create empty state component

---
+++ src/components/EmptyState.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { DocumentMinusIcon, DocumentMagnifyingGlassIcon } from "@heroicons/vue/24/outline";
+defineProps({
+  title: { type: String, default: "Empty state" },
+  text: String,
+  type: {
+    type: String,
+    default: "auction",
+    validator: (value) => ["auction", "search"].includes(value)
+  }
+});
+</script>
+<template>
+  <div class="mx-auto grid max-w-xl place-items-center py-10 text-center">
+    <DocumentMagnifyingGlassIcon
+      v-if="type === 'search'"
+      class="m-5 mt-0 h-10 w-10 text-sm [&>path]:stroke-[0.7]"
+    />
+    <DocumentMinusIcon
+      v-if="type === 'auction'"
+      class="m-5 mt-0 h-10 w-10 text-sm [&>path]:stroke-[0.7]"
+    />
+    <h2 class="mt-2">{{ title }}</h2>
+    <p class="mt-2 text-grey-500">{{ text }}</p>
+    <slot></slot>
+  </div>
+</template>
