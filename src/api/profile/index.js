e0851c9457a72b65b4334c88be8fc0df66cfdd90
Marte <marte.mk@hotmail.com>
2024-04-17 10:46:38 +0000

    Create fetch function for getting user wins

--- src/api/profile/index.js
+++ src/api/profile/index.js
@@ -1,9 +1,11 @@
 import { getProfile } from "./getProfile";
 import { getUserAuctions } from "./getUserAuctions";
 import { updateAvatar } from "./updateAvatar";
+import { getWins } from "./getWins";
 
 export const profile = Object.freeze({
   getProfile,
   getUserAuctions,
-  updateAvatar
+  updateAvatar,
+  getWins
 });
