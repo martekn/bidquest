48682805385f5c5688ee6b575c93666d8f62716a
Marte <marte.mk@hotmail.com>
2024-04-17 10:47:12 +0000

    Create fetch functino for getting user bids

--- src/api/profile/index.js
+++ src/api/profile/index.js
@@ -2,10 +2,12 @@ import { getProfile } from "./getProfile";
 import { getUserAuctions } from "./getUserAuctions";
 import { updateAvatar } from "./updateAvatar";
 import { getWins } from "./getWins";
+import { getBids } from "./getBids";
 
 export const profile = Object.freeze({
   getProfile,
   getUserAuctions,
   updateAvatar,
-  getWins
+  getWins,
+  getBids
 });
