fb394e9be138abbb45711feec34b56fe30217143
Marte <marte.mk@hotmail.com>
2024-04-19 09:53:50 +0000

    Set focus reset to mimic page reload

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 // #region -IMPORTS-
 // Vue-related imports
-import { RouterView } from "vue-router";
+import { nextTick } from "vue";
+import { RouterView, useRouter } from "vue-router";
 
 // Custom components
 import AppNotification from "@/components/AppNotification.vue";
@@ -9,6 +10,30 @@ import AppNavigation from "@/components/AppNavigation.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import SkipToContent from "@/components/SkipToContent.vue";
 // #endregion
+
+const router = useRouter();
+
+router.afterEach((from, to) => {
+  if (
+    to.name === "history" &&
+    from.name === "history" &&
+    (from.params.view === "all" || from.params.view === "wins")
+  ) {
+    return;
+  } else if (
+    to.name === "profile" &&
+    from.name === "profile" &&
+    (from.params.view === "all" || from.params.view === "active")
+  ) {
+    return;
+  } else if (from.path !== to.path) {
+    nextTick(() => {
+      document.body.tabIndex = 0;
+      document.body.focus();
+      document.body.tabIndex = -1;
+    });
+  }
+});
 </script>
 <template>
   <SkipToContent />
