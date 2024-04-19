9a82d8b37a33bfb3e60a7b82e064acaceb1a326e
Marte <marte.mk@hotmail.com>
2024-04-19 20:12:49 +0000

    Include a loader and error dialog for edit page

--- src/views/AuctionFormView.vue
+++ src/views/AuctionFormView.vue
@@ -28,6 +28,7 @@ import ErrorDialog from "@/components/ErrorDialog.vue";
 import LoadingButton from "@/components/LoadingButton.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import ConfirmationDialog from "@/components/ConfirmationDialog.vue";
+import LoadingIndicator from "@/components/LoadingIndicator.vue";
 // #endregion
 
 const route = useRoute();
@@ -36,6 +37,8 @@ const apiError = reactive([]);
 const isLoading = ref(false);
 const isInvalidId = ref(false);
 
+const editHasLoaded = ref(false);
+const editHasError = ref(false);
 const dateValue = ref();
 
 const imageField = ref();
@@ -230,10 +233,14 @@ const getAuction = async () => {
       image.id = nanoid();
       images.push(image);
     }
+    editHasLoaded.value = true;
   } catch (error) {
-    if (error.statusCode === 404 || error.errors[0].code === "invalid_string") {
+    if (error.statusCode === 404 || error?.errors?.[0].code === "invalid_string") {
       isInvalidId.value = true;
     }
+
+    editHasLoaded.value = true;
+    editHasError.value = true;
   }
 };
 
@@ -286,122 +293,137 @@ watch(
   <NotFoundView v-if="isInvalidId"></NotFoundView>
   <main v-else class="bg-straws-pattern">
     <form @submit.prevent novalidate class="mx-auto max-w-2xl px-5 pb-11 pt-6 md:pb-12 md:pt-7">
-      <div class="flex flex-wrap items-center justify-between gap-6">
-        <h1>{{ route.name === "create" ? "Create an auction" : "Edit auction" }}</h1>
-        <button
-          v-if="route.name === 'edit'"
-          @click="isConfirmOpen = true"
-          class="flex items-center gap-2 rounded font-semibold text-grey-500 outline-none transition-all hover:text-red-400 focus-visible:ring-2 focus-visible:ring-black"
-        >
-          <DocumentMinusIcon class="h-5 w-5" /> Delete
-        </button>
-      </div>
-      <ErrorDialog class="mt-7" v-if="apiError.length > 0" title="We could not create auction">
-        <ul class="list-inside list-disc">
-          <li v-for="[error, index] in apiError" :key="index">{{ error.message }}</li>
-        </ul>
-      </ErrorDialog>
-      <div class="mt-7 grid gap-7 md:grid-cols-2">
-        <TextInput
-          v-model="auctionBody.title"
-          id="title"
-          label="Title"
-          type="text"
-          :is-error="titleField.isError"
-          :error="titleField.error"
-          class="md:col-span-full"
-        />
-
-        <TextInput
-          v-model="dateValue"
-          label="End date and time"
-          id="date"
-          type="datetime-local"
-          :min="minDateTime"
-          :max="maxDateTime"
-          :is-error="dateField.isError"
-          :error="dateField.error"
-          :disabled="route.name === 'edit'"
-          class="flex-grow"
-        />
-
-        <ListBox
-          placeholder="Select category"
-          v-model="selectedCategory"
-          :options="categoryOptions"
-          optional
-          id="category"
-          label="Category"
-        />
-
-        <TextareaInput
-          v-model="auctionBody.description"
-          label="Description"
-          id="description"
-          :is-error="descriptionField.isError"
-          :error="descriptionField.error"
-          optional
-          class="h-[17rem] md:col-span-full"
-        />
-      </div>
-      <section class="mb-7 mt-9 border-b border-b-grey-300">
-        <h2>Images <span class="font-normal text-grey-500">(Optional)</span></h2>
-        <p class="mt-3 text-grey-500">
-          To designate the main image for the auction, simply use the first image listed. You can
-          rearrange the images by using the arrow buttons to adjust their order.
-        </p>
-        <div
-          class="mt-7 grid grid-cols-[1fr_auto] gap-4"
-          :class="{
-            ' border-b border-b-grey-300 pb-7': images.length > 0,
-            'mb-7': images.length === 0
-          }"
-        >
+      <template v-if="route.name === 'create' || (editHasLoaded && !editHasError)">
+        <div class="flex flex-wrap items-center justify-between gap-6">
+          <h1>{{ route.name === "create" ? "Create an auction" : "Edit auction" }}</h1>
+          <button
+            v-if="route.name === 'edit'"
+            @click="isConfirmOpen = true"
+            class="flex items-center gap-2 rounded font-semibold text-grey-500 outline-none transition-all hover:text-red-400 focus-visible:ring-2 focus-visible:ring-black"
+          >
+            <DocumentMinusIcon class="h-5 w-5" /> Delete
+          </button>
+        </div>
+        <ErrorDialog class="mt-7" v-if="apiError.length > 0" title="We could not create auction">
+          <ul class="list-inside list-disc">
+            <li v-for="[error, index] in apiError" :key="index">{{ error.message }}</li>
+          </ul>
+        </ErrorDialog>
+        <div class="mt-7 grid gap-7 md:grid-cols-2">
           <TextInput
-            :disabled="images.length >= maxImageCount"
-            :is-error="imageFieldError"
-            v-model="imageField"
-            id="image"
-            label="Image url"
-            type="url"
+            v-model="auctionBody.title"
+            id="title"
+            label="Title"
+            type="text"
+            :is-error="titleField.isError"
+            :error="titleField.error"
+            class="md:col-span-full"
+          />
+
+          <TextInput
+            v-model="dateValue"
+            label="End date and time"
+            id="date"
+            type="datetime-local"
+            :min="minDateTime"
+            :max="maxDateTime"
+            :is-error="dateField.isError"
+            :error="dateField.error"
+            :disabled="route.name === 'edit'"
             class="flex-grow"
           />
-          <button
-            :disabled="images.length >= maxImageCount"
-            class="button button-secondary self-end py-[calc(0.5rem+1px)] leading-normal"
-            @click="addImage"
+
+          <ListBox
+            placeholder="Select category"
+            v-model="selectedCategory"
+            :options="categoryOptions"
+            optional
+            id="category"
+            label="Category"
+          />
+
+          <TextareaInput
+            v-model="auctionBody.description"
+            label="Description"
+            id="description"
+            :is-error="descriptionField.isError"
+            :error="descriptionField.error"
+            optional
+            class="h-[17rem] md:col-span-full"
+          />
+        </div>
+        <section class="mb-7 mt-9 border-b border-b-grey-300">
+          <h2>Images <span class="font-normal text-grey-500">(Optional)</span></h2>
+          <p class="mt-3 text-grey-500">
+            To designate the main image for the auction, simply use the first image listed. You can
+            rearrange the images by using the arrow buttons to adjust their order.
+          </p>
+          <div
+            class="mt-7 grid grid-cols-[1fr_auto] gap-4"
+            :class="{
+              ' border-b border-b-grey-300 pb-7': images.length > 0,
+              'mb-7': images.length === 0
+            }"
           >
-            Add
-          </button>
-          <span
-            v-if="images.length >= maxImageCount || imageFieldError"
-            :class="{ 'text-grey-500': !imageFieldError, 'text-red-400': imageFieldError }"
-            class="col-span-full mt-2 flex gap-2 text-sm leading-tight"
-            :id="'input-error-' + id"
-            ><XCircleIcon v-if="imageFieldError" class="h-5 w-5 flex-shrink-0 leading-tight" />
-            {{ imageFieldMessage }}</span
+            <TextInput
+              :disabled="images.length >= maxImageCount"
+              :is-error="imageFieldError"
+              v-model="imageField"
+              id="image"
+              label="Image url"
+              type="url"
+              class="flex-grow"
+            />
+            <button
+              :disabled="images.length >= maxImageCount"
+              class="button button-secondary self-end py-[calc(0.5rem+1px)] leading-normal"
+              @click="addImage"
+            >
+              Add
+            </button>
+            <span
+              v-if="images.length >= maxImageCount || imageFieldError"
+              :class="{ 'text-grey-500': !imageFieldError, 'text-red-400': imageFieldError }"
+              class="col-span-full mt-2 flex gap-2 text-sm leading-tight"
+              :id="'input-error-' + id"
+              ><XCircleIcon v-if="imageFieldError" class="h-5 w-5 flex-shrink-0 leading-tight" />
+              {{ imageFieldMessage }}</span
+            >
+          </div>
+          <ImageManager
+            :images="images"
+            @moveUp="moveImageUp"
+            @moveDown="moveImageDown"
+            @delete="deleteImage"
+          ></ImageManager>
+        </section>
+        <div class="flex flex-col gap-5">
+          <span class="text-sm text-grey-500" v-if="!requiredFilled"
+            >You need to fill out title and end date before you can continue</span
+          >
+
+          <LoadingButton
+            @buttonClicked="validate"
+            :buttonLoading="isLoading"
+            :disabled="!requiredFilled"
+          >
+            {{ route.name === "create" ? "Create Auction" : "Save Auction" }}</LoadingButton
           >
         </div>
-        <ImageManager
-          :images="images"
-          @moveUp="moveImageUp"
-          @moveDown="moveImageDown"
-          @delete="deleteImage"
-        ></ImageManager>
-      </section>
-      <div class="flex flex-col gap-5">
-        <span class="text-sm text-grey-500" v-if="!requiredFilled"
-          >You need to fill out title and end date before you can continue</span
-        >
-
-        <LoadingButton
-          @buttonClicked="validate"
-          :buttonLoading="isLoading"
-          :disabled="!requiredFilled"
-        >
-          {{ route.name === "create" ? "Create Auction" : "Save Auction" }}</LoadingButton
-        >
-      </div>
+      </template>
+      <LoadingIndicator
+        v-else-if="!editHasLoaded && route.name === 'edit'"
+        class="my-12"
+        color="dark"
+      />
+      <ErrorDialog v-if="editHasLoaded && editHasError" title="Unable to load auction edit form">
+        <p>
+          We apologize for the inconvenience, but we're currently unable to fetch the edit auction
+          form. Please ensure you have a stable internet connection and try refreshing the page. If
+          the issue persists, don't hesitate to contact our support team for assistance. Thank you
+          for your patience and understanding.
+        </p></ErrorDialog
+      >
     </form>
     <ConfirmationDialog
       :isOpen="isConfirmOpen"
