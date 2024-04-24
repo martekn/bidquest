c84aec117645fef0d3718b724180f17534fd8e95
Marte <marte.mk@hotmail.com>
2024-04-24 10:55:23 +0000

    Set restFocus as a helper function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 // #region -IMPORTS-
 // Vue-related imports
-import { nextTick } from "vue";
-import { RouterView, useRouter } from "vue-router";
+import { RouterView } from "vue-router";
+
+// Custom module/helper imports
+import { resetFocus } from "./helper/resetFocus";
 
 // Custom components
 import AppNotification from "@/components/AppNotification.vue";
@@ -12,29 +14,7 @@ import SkipToContent from "@/components/SkipToContent.vue";
 import FadeTransition from "@/components/FadeTransition.vue";
 // #endregion
 
-const router = useRouter();
-
-router.afterEach((from, to) => {
-  if (
-    to.name === "history" &&
-    from.name === "history" &&
-    (from.params.view === "all" || from.params.view === "wins")
-  ) {
-    return;
-  } else if (
-    to.name === "profile" &&
-    from.name === "profile" &&
-    (from.params.view === "all" || from.params.view === "active")
-  ) {
-    return;
-  } else if (from.path !== to.path) {
-    nextTick(() => {
-      document.body.tabIndex = 0;
-      document.body.focus();
-      document.body.tabIndex = -1;
-    });
-  }
-});
+resetFocus();
 </script>
 <template>
   <SkipToContent />
